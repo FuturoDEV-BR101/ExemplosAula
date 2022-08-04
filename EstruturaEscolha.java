5a90f5941b3f611595f933627d8b32eaa508da14
Elder Pereira <eldercesar.pontonet@gmail.com>
2022-08-04 19:37:24 -0300

    Estrutura de Escolha
    Exemplo de utilização da estrutura de escolha IF - ELSE e Operador Ternário

---
+++ EstruturaEscolha.java
@@ -0,0 +1,26 @@
+public class EstruturaEscolha {
+    
+    //Exemplo de utilização da estrutura de escolha IF ELSE e Operador Ternário
+    public static void main(String[] args) {
+        
+        String nome = "Elder";
+        double media = 7.0;
+
+        //utilização do IF sem ELSE
+        if ( media < 7 ) {
+            System.out.println("Não Aprovado!");
+        }
+
+        //utilização do IF com ELSE
+        if ( media < 7 ){
+            System.out.println("Não Aprovado!");
+        } else {
+            System.out.println("Aprovado!");
+        }
+
+        //Utilização do Operador Ternário
+        System.out.println( (media < 7 ) ? "Não Aprodado!" : "Aprovado!" );
+    }
+
+
+}
